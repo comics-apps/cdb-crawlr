4959d94dc81f4c04ed15f5eef635a399b24e60e5
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2018-06-17 12:42:20 +0200

    Return publisher id from single series page

--- lib/cdb/series.rb
+++ lib/cdb/series.rb
@@ -1,5 +1,5 @@
 module CDB
-  class Series < Struct.new(:cdb_id, :name, :publisher, :imprint, :start_date, :end_date, :issues, :country, :language)
+  class Series < Struct.new(:cdb_id, :name, :publisher_id, :publisher_name, :imprint, :start_date, :end_date, :issues, :country, :language)
     FORM_SEARCHTYPE = 'Title'
     WEB_PATH = 'title.php'
 
@@ -19,18 +19,20 @@ module CDB
         name = text.slice(0..-8)
         year = text.slice(-5..-2)
         pub = link.next_sibling.text.gsub(/^\s*\(|\)\s*$/, '')
-        new(:cdb_id => id, :name => name, :publisher => pub, :start_date => year)
+        new(:cdb_id => id, :name => name, :publisher_name => pub, :start_date => year)
       end.sort_by(&:cdb_id)
     end
 
     def self.parse_data(id, page)
       dates = page.css('strong:contains("Publication Date: ")').first.next_sibling.text.strip
       start_d, end_d = dates.split('-').map(&:strip)
+      publisher = page.css('a[href^="publisher.php"]').first
 
       series = new(
         :cdb_id => id.to_i,
         :name => page.css('.page_headline').first.text.strip,
-        :publisher => page.css('a[href^="publisher.php"]').first.text.strip,
+        :publisher_id => publisher.attr('href').split('=').last.to_i,
+        :publisher_name => publisher.text.strip,
         :imprint => (page.css('a[href^="imprint.php"]').first.text.strip rescue nil),
         :start_date => start_d,
         :end_date => end_d,
